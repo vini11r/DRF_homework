3ea32362b9ff27ce745adc13f2e2bab1a4e416c3
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-08-08 21:31:47 +0300

    создал модель подписки

--- materials/models.py
+++ materials/models.py
@@ -61,3 +61,15 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Пользователь', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, verbose_name='Курс в подписке', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} - {self.course}'
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
